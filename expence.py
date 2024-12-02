8a4d774ab615dc30ddfafa4947fff841e12a88e0
sivaprasad2848 <sivaprasad@cybersquare.org>
2024-12-02 13:19:50 +0530

    Expence Modified with date based total calculation

--- expence.py
+++ expence.py
@@ -3,6 +3,7 @@ y=0
 while(y==0):
     print("1-> For insert an expence")
     print("2-> For display an expence")
+    print("3-> For display an expence")
     option=int(input("Enter the option"))
     if(option==1):
         title=input("Enter the title")
@@ -11,6 +12,14 @@ while(y==0):
         expences.append((title,amount,edate))
     if(option==2):
         print(expences)
+    if(option==3):
+        total=0
+        dt=input("Enter a date")
+        for item in expences:
+            if(item[2]==dt):
+                total=total+int(item[1])
+        print("Total amount for the entered date:"+str(total))
+                
 
     y=int(input("Do you want to continue? 0 for yes"))
 
